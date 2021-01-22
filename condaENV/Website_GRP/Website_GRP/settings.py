0789b39fca4fc1352e26732f760dfbe225487b8d
Ivan C Halim <scyih1@nottingham.edu.cn>
2021-01-22 16:50:16 +0700

    [Addition]
    MariaDB/MySQL Database connection.
    
    Connected Django to mariaDB database in setting.py (to login to admin page check the info on settings.py near DATABASES). Use XAMMP or WAMP to host the database and then launch the website. Database is still empty, except for the automated additions done by django.
    
    NOTES :
     - Not sure how to send Databases through git.

--- condaENV/Website_GRP/Website_GRP/settings.py
+++ condaENV/Website_GRP/Website_GRP/settings.py
@@ -76,11 +76,18 @@ WSGI_APPLICATION = 'Website_GRP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+#USER AND NAME IS NOT CHANGE FOR NOW, cause too annoying to set individually - Ivan.
+#Django admin login
+    # Username : gen-o-grp-2020
+    # Password : PainAuChocolate
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Website_Database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 

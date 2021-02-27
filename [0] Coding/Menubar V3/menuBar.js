62e78df77818312554478a6432fd5ebf14ad87e6
Aurelie <scyau1@nottingham.edu.cn>
2021-02-27 16:52:16 +0800

    CODES: menuBar
    added the darken backscreen when the sidebar is opened

--- [0] Coding/Menubar V3/menuBar.js
+++ [0] Coding/Menubar V3/menuBar.js
@@ -1,9 +1,11 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "290px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)"; //added
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white"; //added
 }

808403237ba2c2887b51dae7076da5bb07a91aa0
Aurelie <scyau1@nottingham.edu.cn>
2021-03-16 18:16:48 +0800

    CODES: ProjectUploadPage
    tags dropdown choice instead of the searching of the tags

--- [0] Coding/UploadVideoPage/videouploadify.js
+++ [0] Coding/UploadVideoPage/videouploadify.js
@@ -125,7 +125,7 @@ $('#artefact_desc').keyup(function() {
 				for(let index=0;index<files.length;++index){
 
 					//restricting file size
-          			if(files[index].size > 5000) {
+          			if(files[index].size > 524288000) {
             			alert("File is too big!");
             			break;
           			}

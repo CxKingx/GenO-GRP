2724efcb50480483ff83774a92868ee56b9f8c76
Joseph Then <scyjt1@nottingham.edu.cn>
2021-03-16 15:28:04 +0800

    FEAT(ImageUpload): Fixed and tested

--- condaENV/Website_GRP/home_page/views.py
+++ condaENV/Website_GRP/home_page/views.py
@@ -311,13 +311,14 @@ def searchbar(request):
         post = Video_Artefact.objects.all().filter(Q(name__icontains=search))
         return render(request, 'home_page/searchbar.html', {'post': post})
 
+
 def image_upload_view(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             img_obj = form.instance
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+            return render(request, 'home_page/uploadimage.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
 

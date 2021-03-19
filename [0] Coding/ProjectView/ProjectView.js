d8c9d9602bf360e96c38b96c19ef9b66f64910ec
Aurelie <scyau1@nottingham.edu.cn>
2021-03-19 17:16:41 +0800

    CODES: ProjectView
    artefacts cards-enlarging. Some problems need to be fixed

--- [0] Coding/ProjectView/ProjectView.js
+++ [0] Coding/ProjectView/ProjectView.js
@@ -47,4 +47,35 @@ $(document).ready(function () {
     });
 });
 
+/* enlarging image of artefact cards */
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', afterLoaded);
+} else {
+    //The DOMContentLoaded event has already fired. Just run the code.
+    afterLoaded();
+}
 
+function afterLoaded(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var imgem = document.getElementById("myImg");
+    var imgem2 = document.getElementById("flipcardID");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    imgem2.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = imgem.src;
+        captionText.innerHTML = imgem.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        console.log("test");
+        modal.style.display = "none";
+    }
+}
